9893f4ef369cba7f95fec4223d73ddfa657f4bd7
JPalacio3 <joelpalacio630@gmail.com>
2025-05-17 17:42:04 -0600

    Implementación de Type Guards para el tipado de la respuesta de la API

--- src/components/hooks/useWeather.ts
+++ src/components/hooks/useWeather.ts
@@ -1,13 +1,24 @@
 import axios from "axios";
-import type { SearchType } from "../../types";
+import type { SearchType, Weather } from "../../types";
+
+//  Type Guard o Assertion
+function isWeatherReesponse(weather: unknown) : weather is Weather {
+  return (
+    Boolean(weather) &&
+    typeof weather === "object" &&
+    typeof (weather as Weather).name === "string" &&
+    typeof (weather as Weather).main.temp === "number" &&
+    typeof (weather as Weather).main.temp_min === "number" &&
+    typeof (weather as Weather).main.temp_max === "number"
+  );
+}
 
 export default function useWeather() {
   const fetchWeather = async (search: SearchType) => {
     const appId = import.meta.env.VITE_API_KEY;
 
     try {
-      const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}
-      `;
+      const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`;
 
       const { data } = await axios.get(geoUrl);
 
@@ -16,7 +27,14 @@ export default function useWeather() {
 
       const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`;
 
-      console.log(weatherUrl);
+      // Type Guards
+      const { data: weatherResult } = await axios.get(weatherUrl);
+      const result = isWeatherReesponse(weatherResult);
+
+      if(result) {
+
+      }
+
     } catch (error) {
       console.error("Error fetching weather data:", error);
     }
