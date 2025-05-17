716016141d546829ddbdde19a1a54b6538b35ea1
JPalacio3 <joelpalacio630@gmail.com>
2025-05-17 16:39:43 -0600

    Realizando el llamado a la API completa, obteniendo la respuesta esperada para la consulta del clima en tiempo real

--- src/components/hooks/useWeather.ts
+++ src/components/hooks/useWeather.ts
@@ -6,10 +6,17 @@ export default function useWeather() {
     const appId = import.meta.env.VITE_API_KEY;
 
     try {
-      const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`;
+      const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}
+      `;
 
       const { data } = await axios.get(geoUrl);
-      console.log(data);
+
+      const lat = data[0].lat;
+      const lon = data[0].lon;
+
+      const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`;
+
+      console.log(weatherUrl);
     } catch (error) {
       console.error("Error fetching weather data:", error);
     }
